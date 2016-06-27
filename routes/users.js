3f2e3a2ca7585f81a348dde57519d3ccf975d140
Ed <ek2000@gmail.com>
2016-06-27 14:55:59 -0500

    updates delete and edit routes for user

--- routes/users.js
+++ routes/users.js
@@ -11,30 +11,55 @@ function loggedInUser(req, res, next){
   }
 }
 
-router.get('/users', function(req, res){
+router.get('/', function(req, res){
   Users.then(function(result, err){
     res.render('/users/index', {users: result});
   });
 });
 
 router.get('/new', function(req, res){
-  res.render('/users/new');
+  res.render('users/new');
 });
 
-router.get('/users/:id/', loggedInUser, function(req, res){
+router.get('/:id/edit', loggedInUser, function(req, res){
+  var userId = req.params.id;
+  var authId = req.sessions.userId;
+  Users.where('id', userid).first().then(function(result, err){
+    var user = result;
+    res.render('users/edit', {user: user});
+  });
+});
+
+router.put('/:id', function(req, res){
+
+})
+
+router.delete('/:id', loggedInUser, function(req, res){
   var userId = req.params.id;
   var authId = req.sessions.userId;
   Users.where('id', userId).first().then(function(result, err){
     var user = results;
     if(userId === authId || user.admin){
-      res.render('/users/:id', {user: user});
+      Users.where('id', userId).first().del().then(function(result, err){
+        res.render('users');
+      })
+    }else{
+      res.send('You are not authorized');
+    }
+  });
+});
+
+router.get('/:id', loggedInUser, function(req, res){
+  var userId = req.params.id;
+  var authId = req.sessions.userId;
+  Users.where('id', userId).first().then(function(result, err){
+    var user = results;
+    if(userId === authId || user.admin){
+      res.render('users/profile', {user: user});
+    }else{
+      res.redirect('signin');
     }
   })
 });
-
-router.delete('/users/:id', loggedInUser, function(req, res){
-
-})
-
 
 module.exports = router;
