79435ee15cc1be8e33c51b0965526607940778b6
Ed <ek2000@gmail.com>
2016-06-28 15:59:55 -0500

    updates migrations

--- migrations/20160625132707_fighters_table.js
+++ migrations/20160625132707_fighters_table.js
@@ -3,8 +3,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('fighters', function(table){
     table.increments();
     table.string('slack_name');
-    table.integer('user_id').unsigned().index().references('users.id').onDelete('CASCADE');
-    table.integer('weapon_id').unsigned().index().references('weapons.id').onDelete('CASCADE');
     table.integer('wins');
     table.integer('losses');
     table.string('img_url');
