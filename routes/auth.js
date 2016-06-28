1834b963e85691d67aecf5b7c4434ca834964de4
Ed <ek2000@gmail.com>
2016-06-28 13:51:21 -0500

    adds weapons routes, some auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+router.get('/facebook', passport.authentication('facebook', {state: 'SOME STATE'}), function(req, res){
 
+});
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/';)
+});
 
 
 module.exports = router;
