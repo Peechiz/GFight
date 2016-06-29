799ccd58b9406f9e58e5e4732073188bb6442d54
Chris Impicciche <peechiz@bakudan.local>
2016-06-29 14:20:10 -0500

    adds weapons.sql file to seed with

---
+++ weapons.sql
@@ -0,0 +1,2 @@
+INSERT INTO weapons (weapon,strength) VALUES ('knife',2),('sword',5),('axe body spray',1),('flute',1),('soda can',2);
+INSERT INTO weapons (weapon,strength) VALUES ('chair',2),('spear',5),('wet towel',1),('biting insults',1),('really sharp toothpick',2);
