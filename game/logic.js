26a29a41cec4ad09711cdb8a606eb20cb5f607eb
Chris Impicciche <peechiz@bakudan.local>
2016-06-30 11:46:24 -0500

    fixes syntax errors

--- game/logic.js
+++ game/logic.js
@@ -142,7 +142,7 @@ var game = {
         // attacker win
         return {
           winner: user1,
-          loser: user2
+          loser: user2,
           dead: user2_fighter,
           alive: user1_fighter
         };
@@ -222,12 +222,12 @@ var game = {
             })
             .then(result2 => {
               console.log(result2);
-              resolve(return true)
+              resolve(true)
             })
           })
         }
         else {
-          resolve(return false)
+          resolve(false)
         }
 
       })
@@ -248,7 +248,7 @@ var game = {
       // assign new fighter
       var newFighter = new Promise((resolve,reject)=>{
         resolve(
-          this.getFighter(fighter.loser);
+          this.getFighter(fighter.loser)
         )
       })
 
