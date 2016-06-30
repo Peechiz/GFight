2db2f66e2c61e513aae8becae723fa3e504b6b44
Chris Impicciche <peechiz@bakudan.local>
2016-06-30 16:41:39 -0500

    works on geting fight logic wired up

--- routes/fight.js
+++ routes/fight.js
@@ -3,12 +3,59 @@ var router = express.Router();
 var knex = require('../db/knex');
 var game = require('../game/logic');
 
+// in future, this should maybe pass u1 ( and uf1? )from the session
+router.get('/:u1/:uf1', function(req, res){
+  var user1 = req.params.u1;
+  var userFighter1 = req.params.uf1;
 
-router.get('/', function(req, res){
-  knex('users').then(function(result, err){
-    res.render('fight/select', {user: result});
+  var getAllUsers = knex('users')
+  var getUser1 = knex('users').where('id',user1).first()
+  var getUserFighter1 = knex('fighters').where('id',userFighter1).first()
+
+  Promise.all([getAllUsers,getUser1,getUserFighter1])
+    .then(values => {
+      res.render('fight/select', {
+        users: values[0],
+        user1: values[1],
+        userFighter1: values[2]
+        });
+    })
+})
+
+router.post('/:u1/:uf1', function(req, res){
+  var user1 = req.params.u1;
+  var userFighter1 = req.params.uf1;
+
+  var user2 = req.body.opponent;
+  console.log('user2:',user2);
+  new Promise((resolve,reject)=>{
+    resolve(
+      game.getOpponentFighter(user2)
+    )
+  }).then((userFighter2)=>{
+
+    var getUser1 = knex('users').where('id',user1).first()
+    var getUser2 = knex('users').where('id',user2).first()
+    var getUserFighter1 = knex('fighters').where('id',userFighter1).first()
+    var getUserFighter2 = knex('fighters').where('id',userFighter2).first()
+
+    Promise.all([getUser1,getUser2,getUserFighter1,getUserFighter2])
+    .then(values => {
+      console.log('in the promise.all');
+      console.log('values',values);
+      res.render('fight/ready', {
+        user1: values[0].id,
+        user2: values[1].id,
+        userFighter1: values[2].id,
+        userFighter2: values[3].id
+      })
+    }, reason => {
+      console.log(reason);
+    })
   })
 })
+
+
 
 router.post('/:user1/:user1_fighter/:user2/:user2_fighter', function(req, res){
   var userId1 = req.params.user1;
@@ -18,7 +65,7 @@ router.post('/:user1/:user1_fighter/:user2/:user2_fighter', function(req, res){
 
   var outcome = game.fight(userId1,userFighter1,userId2,userFighter2);
 
-  res.redirect('fight/win', {outcome:outcome});
+  res.render('fight/win', {outcome:outcome});
 })
 
 router.post('/:id/buy', function(req, res){
