82b0ce64ea11cbea522218c9d14d0f901af11e8a
Chris Impicciche <peechiz@bakudan.local>
2016-06-30 16:39:46 -0500

    comments out auth stuff

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-router.get('/facebook', passport.authentication('facebook', {state: 'SOME STATE'}), function(req, res){
-
-});
-
-router.get('/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/',
-  failureRedirect: '/'
-}));
+// router.get('/facebook', passport.authentication('facebook', {state: 'SOME STATE'}), function(req, res){
+//
+// });
+//
+// router.get('/facebook/callback', passport.authenticate('facebook', {
+//   successRedirect: '/',
+//   failureRedirect: '/'
+// }));
 
 router.get('/logout', function(req, res){
   req.logout();
